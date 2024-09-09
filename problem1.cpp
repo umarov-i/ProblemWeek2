038cb450c5f13c40c0bfc90cedbfc7a55141edbb
umarov-i <i.umarov@newuu.uz>
2024-09-09 21:23:33 +0500

    problem1

---
+++ problem1.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+
+int main() {
+    int a;
+    int b;
+    int c;
+
+    std::cin >> a >> b >> c;
+
+    int d = a - b - c;
+    int e = b * c - a;
+    int f = 2 * b - c + 5 * a;
+
+    std::cout << d << "\n";
+    std::cout << e << "\n";
+    std::cout << f << "\n";
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
