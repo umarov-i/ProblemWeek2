56a2417fa5506b31054ec5c112acd78a7d5115d7
umarov-i <i.umarov@newuu.uz>
2024-09-10 15:52:29 +0500

    problem2

---
+++ problem2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    double d1;
+    double d2;
+
+    std::cin >> d1 >> d2;
+
+    double area = (d1 * d2) / 2;
+
+    std::cout << area << "\n";
+    return 0;
+}
